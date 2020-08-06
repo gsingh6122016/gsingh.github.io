5c0a547e48649aa2de3672e911ce722072a6798b
gourav singh <gsingh6122016@gmail.com>
2020-08-06 13:55:26 +0530

    added event listener and the modularity

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let objArr = [];
 
+
+
 class Address {
     constructor() {
         
@@ -15,7 +17,6 @@ class Address {
     }
 
     setAddressValue(id ,name, strName, hosNo, buildType, aprtName,  cityName, state, pincode) {
-
         this.id = id;
         this.name = name;
         this.strName=strName;
@@ -24,15 +25,21 @@ class Address {
         this.aprtName = aprtName;
         this.cityName = cityName;
         this.state = state;
-        this.pincode = pincode; 
-        
+        this.pincode = pincode;     
+    }
+}
+
+
+
+
+
+class Directory {
+    constructor(viewObj) {
+        this.viewObj=viewObj;
     }
 
- displayFIlterData(objectDataArr) {
-
+    displayFIlterData(objectDataArr) {
     const mySection = document.querySelector('section');
-
-    
       mySection.innerHTML = "";
 
       if(objectDataArr.length === 0){
@@ -45,6 +52,8 @@ class Address {
         for(let i=0; i<objectDataArr.length; i++) {
             const myArticle = document.createElement('article');
     
+            
+            
             const id = document.createElement('p');
             const Name = document.createElement('p');
             const streetname = document.createElement('p');
@@ -76,24 +85,22 @@ class Address {
             myArticle.appendChild(pincode);
             mySection.appendChild(myArticle);
       }
-
-
-      
       }
-
-
-   
-
 }
 
 
- fetchAddById  ()  {
-    var id = document.getElementById("form2");
+ fetchAddById = () => {
+
+    if(!this.validateForm(this.viewObj.form2.elements[0].value)){
+        return;
+    }
+
     var objectDataArr = [];
+  
 
     for(let i=0; i<objArr.length; i++) {
         
-       if (objArr[i].id == id.elements["searchid"].value ) {
+       if (objArr[i].id == this.viewObj.searchidInput.value ) {
            objectDataArr.push(objArr[i]);
           this.displayFIlterData(objectDataArr);
        }
@@ -103,117 +110,165 @@ class Address {
 }
 
 
- fetchAddByApp ()  {
-    var id = document.getElementById("form3");
-    let newArr = [];
-    for(let i=0; i<objArr.length; i++) {
-       if (objArr[i].aprtName === id.elements["searchapartment"].value && id.elements["searchapartment"].value !== "") {
-           newArr.push(objArr[i]);
-       }
 
-   }   
-   
-   this.displayFIlterData(newArr);
-}
-
-
- fetchAddByPin () {
-    var id = document.getElementById("form4");
-    let newArr = [];
-    for(let i=0; i<objArr.length; i++) {
-       if (objArr[i].pincode === id.elements["searchpincode"].value) {
-           newArr.push(objArr[i]);
-       }
-
-   }   
-   this.displayFIlterData(newArr);
-}
-
-
-deactivateHandler() {
- 
-    if (event.target.value === "standalone") {     
-     document.getElementsByTagName("INPUT")[3].setAttribute("disabled", "true");
+ fetchAddByApp = () => {
+    if(!this.validateForm(this.viewObj.form3.elements[0].value)){
+        return;
     }
- 
+
+    let newArr = [];
+
+
+    for(let i=0; i<objArr.length; i++) {
+       if (objArr[i].aprtName === this.viewObj.searchapartmentInput.value
+        && this.viewObj.searchapartmentInput.value != "") {
+           newArr.push(objArr[i]);
+       }
+
+   }     
+   this.displayFIlterData(newArr);
+}
+
+
+
+ fetchAddByPin = () => {
+
+    if(!this.validateForm(this.viewObj.form4.elements[0].value)){
+        return;
+    }
+
+    let newArr = [];
+    for(let i=0; i<objArr.length; i++) {
+       if (objArr[i].pincode === this.viewObj.searchpincodeInput.value) {
+           newArr.push(objArr[i]);
+       }
+
+   }   
+   this.displayFIlterData(newArr);
+}
+
+
+
+deactivateHandler = () => {
+
+    if (event.target.value === "standalone") {     
+     this.viewObj.deactivateInput.setAttribute("disabled", "true");
+    }
     else {
-          document.getElementsByTagName("INPUT")[3].removeAttribute("disabled");
+          this.viewObj.deactivateInput.removeAttribute("disabled");
     }
      
  }
  
- showAll() {
+
+ showAll = () => {
      this.displayFIlterData(objArr);
  }
  
 
 
- storeAddress () {
+ storeAddress = () => {
+        
+        for (let j=0;j<this.viewObj.form.elements.length;j++) {
 
-    var oform = document.getElementById("form");
-  
+            if(!this.validateForm(this.viewObj.form.elements[j].value)){
+                return;
+            }           
+        }
 
-
-   let i=localStorage.length/9;
-   
-     
-                
+   let i= Math.floor(localStorage.length/9 ) ;
         localStorage.setItem("ID"+i,Date.now().toString() + Math.floor((Math.random() * 100) + 1) );
-        localStorage.setItem("Name"+i,oform.elements["name"].value );
-        localStorage.setItem("Street Name"+i,oform.elements["streetname"].value );
-        localStorage.setItem("House Number"+i,oform.elements["houseno"].value );
-        localStorage.setItem("Building Type"+i,oform.elements["buildingtype"].value );
-        localStorage.setItem("Apartment Name"+i,oform.elements["aprtname"].value );
-        localStorage.setItem("City Name"+i,oform.elements["cityname"].value );
-        localStorage.setItem("State"+i,oform.elements["state"].value );
-        localStorage.setItem("pincode"+i,oform.elements["pincode"].value );
-       
-        console.log(localStorage);
-        location.reload();
-
-    //   let obj = new Address();
-    //  obj.setAddressValue( 
-    //      localStorage.getItem("ID"+i),
-    //     localStorage.getItem("Name"+i),
-    //  localStorage.getItem("Street Name"+i),
-    //  localStorage.getItem("House Number"+i),
-    //  localStorage.getItem("Building Type"+i),
-    //  localStorage.getItem("Apartment Name"+i), 
-    //  localStorage.getItem("City Name"+i), 
-    //  localStorage.getItem("State"+i),
-    //  localStorage.getItem("pincode"+i)
-
-    //  );
+        localStorage.setItem("Name"+i, this.viewObj.nameInput.value);
+        localStorage.setItem("Street Name"+i,this.viewObj.streetnameInput.value );
+        localStorage.setItem("House Number"+i,this.viewObj.housenoInput.value );
+        localStorage.setItem("Building Type"+i,this.viewObj.buildingtypeInput.value );
+        localStorage.setItem("Apartment Name"+i,this.viewObj.aprtnameInput.value );
+        localStorage.setItem("City Name"+i,this.viewObj.citynameInput.value );
+        localStorage.setItem("State"+i,this.viewObj.stateInput.value );
+        localStorage.setItem("pincode"+i,this.viewObj.pincodeInput.value );
       
+        location.reload(); 
+    
+
+                }
 
 
-    objArr.push(obj);
+validateForm(x) {
 
-    oform.reset();
+    if (x == "") {
+        alert("All fields must be filled out");
+        return false;
+    }
+    else 
+    {
+        return true;
+    }
+
+}
 
 
+}
+
+class View {
+    constructor() {
+        this.nameInput = document.querySelector('#name');
+        this.housenoInput = document.querySelector('#houseno');
+        this.streetnameInput = document.querySelector('#streetname');
+        this.buildingtypeInput = document.querySelector('#buildingtype');
+        this.aprtnameInput = document.querySelector('#aprtname');
+        this.citynameInput = document.querySelector('#cityname');
+        this.stateInput = document.querySelector('#state');
+        this.pincodeInput = document.querySelector('#pincode');
+        
+        this.formButton = document.querySelector('#form-submit');
+        
+        this.searchidInput = document.querySelector('#searchid');
+        this.searchidSubmit = document.querySelector('#searchid-submit');
    
+        this.searchapartmentInput = document.querySelector('#searchapartment');
+        this.searchapartmentSubmit = document.querySelector('#searchapartment-submit');
+        
+        this.searchpincodeInput = document.querySelector('#searchpincode');
+        this.searchpincodeSubmit = document.querySelector('#searchpincode-submit');
+        
+        this.showAllSubmit = document.querySelector('#show-all');
 
-    obj.displayFIlterData(objArr);
+        this.deactivateSelect = document.querySelector('#buildingtype');
+        this.deactivateInput = document.getElementsByTagName("INPUT")[3];
 
+        this.form = document.getElementById("form");
+        this.form2 = document.getElementById("form2");
+        this.form3 = document.getElementById("form3");
+        this.form4 = document.getElementById("form4");
+    }
 }
 
 
+let viewObj = new View()
 
-}
-
-
-
-
-let Obj = new Address();
+let Dobj = new Directory(viewObj);
 
 
+
+
+viewObj.formButton.addEventListener('click', Dobj.storeAddress);
+viewObj.searchidSubmit.addEventListener('click', Dobj.fetchAddById);
+viewObj.searchapartmentSubmit.addEventListener('click', Dobj.fetchAddByApp);
+viewObj.searchpincodeSubmit.addEventListener('click', Dobj.fetchAddByPin);
+viewObj.showAllSubmit.addEventListener('click', Dobj.showAll);
+viewObj.deactivateSelect.addEventListener('change', Dobj.deactivateHandler);
+
+
+
+
+
+//This function exicutes whenever page refreshes.
 function callIt()
 {
     for(let i=0; i<localStorage.length/9; i++ ){
         
-      let obj = new Address();
-      obj.setAddressValue( 
+      let addObj = new Address();
+      addObj.setAddressValue( 
       localStorage.getItem("ID"+i),
       localStorage.getItem("Name"+i),
       localStorage.getItem("Street Name"+i),
@@ -226,13 +281,11 @@ function callIt()
  
       );
        
- 
- 
-     objArr.push(obj);
+     objArr.push(addObj);
  
     }
-    let obj2 = new Address();
-    obj2.displayFIlterData(objArr);
+    
+    Dobj.displayFIlterData(objArr);
   
 }
 window.onload= callIt;
